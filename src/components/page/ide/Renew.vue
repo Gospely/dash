1eb3c5ebc594594efb40e25322e8ef3ab5412644
leinue <597055914@qq.com>
2016-09-27 09:31:42 +0800

    update ide renew

--- src/components/page/ide/Renew.vue
+++ src/components/page/ide/Renew.vue
@@ -48,8 +48,46 @@
                 <div slot="body">
                     <label class="label">续费时长</label>
                     <p class="control">
-                      <input class="input" type="text" placeholder="域名名称">
+                      <a class="button is-small">1个月</a>
+                      <a class="button is-small">2个月</a>
+                      <a class="button is-small">4个月</a>
+                      <a class="button is-small">6个月</a>
+                      <a class="button is-small">1年</a>
+                      <a v-show="isOther == false" class="button is-small" @click="enterEditOtherTime">{{otherTime || 其它}}</a>
+                      <input v-model="otherTime" v-show="isOther == true" class="input" type="text" @keydown.enter="isOther = false" style="position: absolute;width: 40px;height: 24px;box-shadow: none;margin-left:3px" />
                     </p>
+                    <label class="label">选择主版本</label>
+                    <p class="control">
+                      <span class="select">
+                        <select>
+                          <option>个人版</option>
+                          <option>企业版</option>
+                          <option>教育版</option>    
+                        </select>
+                      </span>
+                    </p>
+
+                    <label class="label">选择次版本</label>
+                    <p class="control">
+                      <span class="select">
+                        <select>
+                          <option>旗舰版</option>
+                          <option>Web App版</option>
+                          <option>Hybrid App版</option>    
+                          <option>微信小程序版</option>    
+                        </select>
+                      </span>
+                    </p>
+
+                    <div class="media-content">
+                      <div class="content">
+                        <div class="media-right" style="text-align:right">
+                            <span class="is-tip">合计：</span>
+                            <span class="is-big">1200.00 元</span>
+                        </div>
+                      </div>
+                    </div>
+
                 </div>
                 <div slot="footer">
                     <button class="button is-success"
@@ -74,7 +112,10 @@
         data () {
             return {
                 isRefresh: false,
-                showRenewForm: false
+                showRenewForm: false,
+
+                isOther: false,
+                otherTime: '其它'
             }
         },
         components: {
@@ -91,6 +132,13 @@
 
             confirmRenew: function() {
 
+            },
+
+            enterEditOtherTime: function() {
+              if(this.otherTime == '其它') {
+                this.otherTime = '';
+              }
+              this.isOther = true;
             }
         }
     }
