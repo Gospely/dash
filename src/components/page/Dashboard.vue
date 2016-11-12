4d913acc8432733620d88a086166cbde9bb74c10
SharksevenRo <937257166@qq.com>
2016-11-12 10:58:07 +0800

    update

--- src/components/page/Dashboard.vue
+++ src/components/page/Dashboard.vue
@@ -46,7 +46,7 @@
             </div>
             <div class="column">
                 <p class="notification has-text-centered">
-                    <span class="title">到期时间<br><span class="subtitle">{{expireat | dateFormat 'yyyy-MM-dd hh:mm:ss'}}</span></span>
+                    <span class="title">到期时间<br><span class="subtitle">{{expireat}}</span></span>
                 </p>
             </div>
         </div>
@@ -161,11 +161,29 @@
                               if(data.fields.expireAt == null || data.fields.expireAt == undefined) {
                                 _self.expireat = '免费无限版本';
                               }else{
-                                _self.expireat = data.fields.expireAt;
+                                _self.expireat = dataFormatdate(new Date(data.fields.expireAt),"yyyy-MM-dd");
                               }
 
                           }
                       }
+
+                      function dateFormat(date,fmt){ //author: meizz
+                         var o = {
+                           "M+" : date.getMonth()+1,                 //月份
+                           "d+" : date.getDate(),                    //日
+                           "h+" : date.getHours(),                   //小时
+                           "m+" : date.getMinutes(),                 //分
+                           "s+" : date.getSeconds(),                 //秒
+                           "q+" : Math.floor((date.getMonth()+3)/3), //季度
+                           "S"  : date.getMilliseconds()             //毫秒
+                         };
+                         if(/(y+)/.test(fmt))
+                           fmt=fmt.replace(RegExp.$1, (date.getFullYear()+"").substr(4 - RegExp.$1.length));
+                         for(var k in o)
+                           if(new RegExp("("+ k +")").test(fmt))
+                         fmt = fmt.replace(RegExp.$1, (RegExp.$1.length==1) ? (o[k]) : (("00"+ o[k]).substr((""+ o[k]).length)));
+                         return fmt;
+                     }
                     }
                   });
 
