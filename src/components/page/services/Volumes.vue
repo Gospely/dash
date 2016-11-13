597077acc7b7139c955cbd775a64d927ec763564
SharksevenRo <937257166@qq.com>
2016-11-13 20:16:55 +0800

    update

--- src/components/page/services/Volumes.vue
+++ src/components/page/services/Volumes.vue
@@ -33,14 +33,18 @@
                 <div slot="body">
 
                     <div v-show="changeSize">
+                      <div class="column is-8">
+                          <label class="label">原数据卷大小： {{volume.min}}</label>
+                      </div>
                       <div class="control is-horizontal user-center">
                         <div class="control-label">
                           <label class="label">数据卷大小</label>
                         </div>
                         <div class="control is-grouped" style="margin-left:16px">
                           <div class="columns">
+
                               <div class="column is-8">
-                                  <input v-model="volume.size" value="20" min="10" max="100" step="10" class="slider" type="range" style="margin-top:10px;">
+                                  <input v-model="volume.size" value="20" min="{{volume.min}}" max="{{volume.max}}" step="{{volume.step}}" class="slider" type="range" style="margin-top:10px;">
                               </div>
                               <div class="column is-4">
                                   <span class="help is-tip" style="margin: 8px;">{{volume.size}} G</span>
@@ -53,7 +57,7 @@
                         <div class="content">
                           <div class="media-right" style="text-align:right">
                               <span class="is-tip">合计：</span>
-                              <span class="is-big">1200.00 元</span>
+                              <span class="is-big">{{price}} 元</span>
                           </div>
                         </div>
                       </div>
@@ -63,7 +67,7 @@
 
                       <div>
                           <span>合计：</span>
-                          <span>1200.00 元</span>
+                          <span>{{price }} 元</span>
                       </div>
 
                       <hr class="split">
@@ -79,7 +83,7 @@
 
                 </div>
                 <div slot="footer">
-                    <button class="button is-success" v-show="changeSize" @click="nextStep">下一步</button>
+                    <button class="button is-success" v-show="changeSize" @click="nextStep">确认下单</button>
 
                     <button class="button is-success" v-show="goPay" @click="prvStep">上一步</button>
 
@@ -105,6 +109,8 @@
     import Cyc from '../../ui/Cyc.vue'
     import Modal from '../../ui/Modal/Modal.vue'
     import PayMethod from '../../ui/PayMethod.vue'
+    import uuid from 'node-uuid'
+    import _md5 from 'md5'
 
     export default{
       computed: {
@@ -142,7 +148,16 @@
             all: 8,
             cur: 1,
             qrcode: '',
-
+            volume: {
+              size: 10,
+              min: 10,
+              max: 100,
+              step: 10
+            },
+            orderNo: '',
+            price: '',
+            unitPrice: '',
+            products: '',
             renewIDEVolumeForm: false,
 
             changeSize: true,
@@ -176,36 +191,111 @@
           this.renewIDEVolumeForm = true;
         },
 
-        initVolume: function(cur) {
-
+        init: function() {
             var _self = this;
-
-            services.Common.list(
-              {
-                url: "volumes",
-                ctx: _self,
-                param: {
-                  cur: cur,
-                  limit: 1,
-                  creator: "1"
+            //获取用户当前数据卷大小
+            services.Common.getOne({
+              url: 'users',
+              param: {
+                id: currentUser
+              },
+              cb: function(res) {
+                var data = res.data;
+                console.log(res);
+                if(data.code == 1) {
+                  _self.volume.size = data.fields.volumeSize;
+                  _self.volume.min = data.fields.volumeSize;
+                  if(data.fields.volumeSize>100) {
+                    _self.volume.max = 1000;
+                    _self.volume.step = 100;
+                  }else{
+                    _self.volume.max = 100;
+                    _self.volume.step = 10;
+                  }
                 }
               }
-            );
+            });
+            //获取数据卷的价格
+            services.Common.list({
+              url: 'products',
+              param: {
+                type: 'volume'
+              },
+              cb: function(res) {
+                  var data =  res.data;
+                  if(data.code == 1) {
+                    console.log(data);
+                    _self.unitPrice =  data.fields[0].price;
+                    _self.products = data.fields[0].id;
+                  }
+              }
+            });
         },
         nextStep() {
           this.changeSize = false;
           this.goPay = true;
+          this.orderNo =  _md5(uuid.v4());
+          console.log(this.orderNo);
+          var _self = this;
+          services.Common.create({
+            url: 'orders',
+            param: {
+              creator: currentUser,
+              name: '数据卷扩容',
+              orderNo: this.orderNo,
+              size: this.volume.size - this.volume.min,
+              unit: "GB",
+              price: (this.volume.size - this.volume.min) * this.unitPrice,
+              unitPrice: this.unitPrice
+            },
+            cb: function(res) {
+              if(res.data.code == 1) {
+                notification.alert("下单成功");
+
+                services.OrderService.order({
+
+                  out_trade_no: _self.orderNo,
+                  price: (_self.volume.size - _self.volume.min) * _self.unitPrice,
+                  type: 'wechat'
+                }).then(function(res){
+                    console.log(res);
+                    _self.qrcode = res.data.code_url;
+                    //window.location.href = res.body;
+                },function(err,res){
+
+                });
+              }
+            }
+          });
+
+
         },
         prvStep() {
           this.goPay = false;
           this.changeSize = true;
         },
+        valueChange() {
+          console.log(this.volume.size);
+        },
         confirmRenewIDEVolume() {
+          if(this.isAlipay){
+            services.OrderService.order({
+              out_trade_no: this.orderNo,
+              price: (this.volume.size - this.volume.min) * this.unitPrice,
+              type: "alipay"
+            }).then(function(res){
+                console.log(res);
+                window.location.href = res.body;
+            },function(err,res){
 
+            });
+          }else{
+            notification.alert("请确认微信扫码支付完成");
+          }
         }
       },
       ready: function() {
-          this.$get("initVolume")(1);
+          this.$get("init")();
       },
       events: {
         'weixin': function() {
@@ -217,6 +307,13 @@
            this.isWechat = false;
            this.isAlipay = true;
          }
+      },
+      watch:{
+        'volume.size': function(newVal, oldVal) {
+
+            var total = (newVal - this.volume.min) * this.unitPrice;
+            this.price = (newVal - this.volume.min) +" X " +  this.unitPrice + "GB = " + total;
+        }
       }
     }
 </script>
