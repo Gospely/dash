522480565eaffdb08c932a28b948e1cd6bb7a31d
SharksevenRo <937257166@qq.com>
2016-11-17 11:17:40 +0800

    update:wq

--- src/main.js
+++ src/main.js
@@ -11,7 +11,7 @@ import filter from './filter/index.js'
 
 import 'animate.css'
 
-// if(document.domain == 'localhost') {
+// if(document.domain == 'gospely.com') {
 // 	window.debug = true;
 // }else {
 // 	document.domain = 'dash.dodora.cn';
@@ -25,25 +25,7 @@ if (document.domain != 'localhost') {
 document.title = 'Gospel控制面板 - Dodora 龙猫云';
 window.notification = notification;
 
-window.setCookie = function setCookie(c_name, value, expiredays) {
-  var exdate = new Date()
-  exdate.setDate(exdate.getDate() + expiredays)
-  document.cookie = c_name + "=" + escape(value) +
-    ((expiredays == null) ? "" : ";expires=" + exdate.toGMTString())
-}
 
-window.getCookie = function getCookie(c_name) {
-  if (document.cookie.length > 0) {
-    c_start = document.cookie.indexOf(c_name + "=")
-    if (c_start != -1) {
-      c_start = c_start + c_name.length + 1
-      c_end = document.cookie.indexOf(";", c_start)
-      if (c_end == -1) c_end = document.cookie.length
-      return unescape(document.cookie.substring(c_start, c_end))
-    }
-  }
-  return ""
-}
 
 window.baseUrl = "http://" + window.location.host
   //初始化XMLHttpRequest RestfulAPI
