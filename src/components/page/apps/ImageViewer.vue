c118475be9f772cefc98703545ab5c49410ae77d
SharksevenRo <937257166@qq.com>
2016-11-22 16:33:17 +0800

    update

--- src/components/page/apps/ImageViewer.vue
+++ src/components/page/apps/ImageViewer.vue
@@ -12,6 +12,17 @@
               </div>
             </div>
             <page :cur.sync="cur_gospel" :all.sync="all_gospel" v-on:btn-click="listen_gospel"></page>
+            </tab-item>
+            <tab-item title="快速应用部署">
+            <div class="columns">
+              <div class="column" v-for="item in fields">
+                <div @click="selectThisImage(item)" class="creation-modal-choice" :style="{backgroundImage: 'url(' + item.description + ')'}">
+                  {{item.name}}
+                </div>
+              </div>
+            </div>
+            <page :cur.sync="cur" :all.sync="all" v-on:btn-click="listen"></page>
+            </tab-item>
 	    </tab>
 
     </div>
@@ -68,7 +79,7 @@
 
           listen: function(data) {
             console.log('你点击了'+data+ '页');
-            this.$get('init_docker_hub')(data);
+            this.$get('init_app_hub')(data);
           },
           listen_gospel: function(data) {
             console.log('你点击了'+data+ '页');
@@ -77,13 +88,13 @@
         	selectThisImage: function(item) {
 				        this.$dispatch('imageOnSelected', item);
         	},
-          init_docker_hub: function(cur){
+          init_app_hub: function(cur){
 
               var _self = this;
               var options = {
 
                 param: {
-                  type: 'docker_hub',
+                  type: 'application',
                   limit: 10,
                   cur: cur,
                 },
@@ -115,7 +126,7 @@
         ready: function(){
 
             console.log("init hub");
-            // this.$get("init_docker_hub")(1);
+            this.$get("init_app_hub")(1);
             this.$get("init_gospel_hub")(1);
         }
     }
