5d362366ea50ff877928d4dde7c7347f40ebb5da
SharksevenRo <937257166@qq.com>
2016-12-07 11:15:16 +0800

    update

--- src/components/page/accounts/Register.vue
+++ src/components/page/accounts/Register.vue
@@ -20,7 +20,7 @@
                     <div class="input-field">
                       <input type="text" v-model="authCode" placeholder="验证码" autocapitalize="off" style="border: none;" ></div>
                   </div>
-                  <a class="button" @click="getTelCode" v-show="isPhone" :disabled="btn_disabled" style="position: absolute; margin-top: -42px; height: 41px; right: 30px; border: 1px solid #dfebf2;">{{btn_info}}</a>
+                  <a class="button" @click="getTelCode" :disabled="btn_disabled" style="position: absolute; margin-top: -42px; height: 41px; right: 30px; border: 1px solid #dfebf2;">{{btn_info}}</a>
                   <ul class="error-msg-list"></ul>
                   <button class="signup-form__submit" @click="register" >注册</button>
                   <div class="signup-form-nav">
@@ -126,24 +126,46 @@
           },
           getTelCode: function(){
 
-              console.log("code");
-              var _self = this;
-              services.Common.list({
-                url: 'users/phone/code',
-                param: {
-                  phone: _self.phone,
-                },
-                cb: function(res) {
-                    if(res.status == 200){
-                      var data = res.data;
-                      if(data.code ==1){
-                        console.log(data);
-                        _self.token = data.fields;
-                        notification.alert(data.message);
+              if(this.isPhone){
+                console.log("code");
+                var _self = this;
+                services.Common.list({
+                  url: 'users/phone/code',
+                  param: {
+                    phone: _self.phone,
+                  },
+                  cb: function(res) {
+                      if(res.status == 200){
+                        var data = res.data;
+                        if(data.code ==1){
+                          console.log(data);
+                          _self.token = data.fields;
+                          notification.alert(data.message);
+                        }
                       }
-                    }
-                }
-              });
+                  }
+                });
+              }else{
+                console.log("code");
+                var _self = this;
+                services.Common.list({
+                  url: 'users/email/code',
+                  param: {
+                    email: _self.phone,
+                  },
+                  cb: function(res) {
+                      if(res.status == 200){
+                        var data = res.data;
+                        if(data.code ==1){
+                          console.log(data);
+                          _self.token = data.fields;
+                          notification.alert(data.message);
+                        }
+                      }
+                  }
+                });
+              }
+
               _self.$get("renderButton")();
           },
           renderButton: function() {
