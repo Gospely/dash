46d7df6a2dc310028c341f1ce04f1d7780eed625
SharksevenRo <937257166@qq.com>
2017-01-04 11:16:35 +0800

    update

--- src/services/index.js
+++ src/services/index.js
@@ -7,7 +7,7 @@ var Common = require('./CommonService');
 
 var model = {
 	instance: undefined,
-	// baseUrl: 'http://localhost:8089/'
+	//baseUrl: 'http://localhost:8089/'
 	//baseUrl: 'http://api.gospely.com:9999/'
 	baseUrl: 'http://api.gospely.com/'
 	//baseUrl: 'http://192.168.31.23:9999/'
